dab3b78d5b50c9603e7b0e3e21b6f336e55679f1
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-01-28 21:49:42 +0530

    Blogs page sider

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,5 +1,15 @@
 const BlogsPage = () => {
-	return <div>BlogsPage</div>;
+	return (
+		<div className="bg-gray-900">
+			<div className="h-full flex flex-col items-center justify-start w-64">
+				<li>Hello</li>
+				<li>Hello 2</li>
+				<li>Hello 3</li>
+				<li>Hello 4</li>
+				<li>Hello 5</li>
+			</div>
+		</div>
+	);
 };
 
 export default BlogsPage;
