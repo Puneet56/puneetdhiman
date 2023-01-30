cb2562e5616c12c3636daefaeb8eaaf8cb7e7a01
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-01-30 17:18:20 +0530

    Icons

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
 		<div className="flex items-center justify-around px-8 py-4">
 			<h1 className="font-normal text-2xl">{"{ / }"} puneetdhiman.com</h1>
 			<ul className="flex flex-row gap-8">
-				<li>Work</li>
+				<li className="hover:text-yellow-500">Work</li>
 				<li>About</li>
 				<li>Blog</li>
 				<li>Contact</li>
