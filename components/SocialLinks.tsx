334f3864e5948e51ee1ea7e224e724f1f61dbe8b
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-03-12 17:30:56 +0530

    Header component

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,27 +1,27 @@
-const SocialLinks = (props: { iconSizeClass?: string; className?: string }) => {
-	let data = [
-		{
-			name: 'Facebook',
-			link: 'https://www.facebook.com/puneet.dhiman.127/',
-			image: './assets/facebook.svg',
-		},
-		{
-			name: 'Github',
-			link: 'https://github.com/Puneet56',
-			image: './assets/github.svg',
-		},
-		{
-			name: 'Instagram',
-			link: 'https://www.instagram.com/puneet.dhiman.127/',
-			image: './assets/instagram.svg',
-		},
-		{
-			name: 'Linkedin',
-			link: 'https://www.linkedin.com/in/puneet-dhiman/',
-			image: './assets/linkedin.svg',
-		},
-	];
+let data = [
+	{
+		name: 'Facebook',
+		link: 'https://www.facebook.com/puneet.dhiman.127/',
+		image: './assets/facebook.svg',
+	},
+	{
+		name: 'Github',
+		link: 'https://github.com/Puneet56',
+		image: './assets/github.svg',
+	},
+	{
+		name: 'Instagram',
+		link: 'https://www.instagram.com/puneet.dhiman.127/',
+		image: './assets/instagram.svg',
+	},
+	{
+		name: 'Linkedin',
+		link: 'https://www.linkedin.com/in/puneet-dhiman/',
+		image: './assets/linkedin.svg',
+	},
+];
 
+const SocialLinks = (props: { iconSizeClass?: string; className?: string }) => {
 	return (
 		<div
 			className={`z-30 flex h-12 w-56 flex-row items-center justify-around ${props.className} `}
