985806b54b19dd34a13c151986601a93f1b2ed88
Puneet Dhiman <puneetdhiman56@gmail.com>
2023-03-13 22:13:11 +0530

    got animation working

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,40 +1,22 @@
-import SlideShow from './SlideShow';
+let text = ['D', 'E', 'V', 'E', 'L', 'O', 'P', 'E', 'R'];
 
 const HeroSection = () => {
 	return (
-		<div className="relative flex min-h-screen flex-col items-center justify-center overflow-x-hidden md:flex-row">
-			<div className="my-flip absolute right-0 -top-0">
-				<img src={'/assets/svg-cropped.png'} alt="blob"></img>
-			</div>
-			<div className="relative mt-10 flex h-5/6 w-full flex-col items-start justify-center md:mt-0">
-				<p className="ml-8 mt-12 text-3xl text-gray-100 sm:text-4xl md:mx-10 md:mt-28 md:text-5xl">
-					Hey,
-				</p>
-				<p className="ml-8 mb-1.5 text-5xl text-white sm:text-6xl md:mx-10 md:text-7xl">
-					I'm Puneet
-				</p>
-				<p className="m-1.5 ml-8 text-xl text-gray-100 sm:text-2xl md:mx-10 md:text-3xl">
-					Tech Enthusiast
-				</p>
-				<p className="m-1.5 ml-8 text-xl text-gray-100 sm:text-2xl md:mx-10 md:text-3xl">
-					Love Building Things
-				</p>
-				<div
-					className="min-h-4 mt-8 ml-10 flex h-16 w-36 transform cursor-pointer items-center justify-center
-													rounded-full bg-gradient-to-tr from-blue-600 to-pink-700 p-1 shadow-md 
-													transition hover:scale-90 md:h-20 md:w-48 md:p-1.5"
+		<div className="mt-16 flex items-center justify-center">
+			<span className="text-3xl font-bold text-white md:text-7xl">{'<'}</span>
+			<span className="text-3xl font-bold text-white md:text-7xl">{'/'}</span>
+			{text.map((letter, index) => (
+				<span
+					key={index}
+					className="animate-pop text-4xl text-white md:text-7xl"
+					style={{
+						animationDelay: `${index * 0.1}s`,
+					}}
 				>
-					<a
-						href="https://www.linkedin.com/in/puneet-dhiman/"
-						target="_blank"
-						rel="noreferrer"
-						className="flex h-full w-full items-center justify-center rounded-full bg-mycolor text-lg font-bold text-white"
-					>
-						Connect
-					</a>
-				</div>
-			</div>
-			<SlideShow />
+					{letter}
+				</span>
+			))}
+			<span className="text-3xl font-bold text-white md:text-7xl">{'>'}</span>
 		</div>
 	);
 };
