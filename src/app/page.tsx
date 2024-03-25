61d1e3ddcd6229c7918371fcfd0ab7990c4bfcff
Puneet Dhiman <puneetdhiman56@gmail.com>
2024-03-25 21:53:09 +0530

    Init

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="container mx-auto py-20">
+      <div className="flex items-start justify-between">
+        <div>
+          <h1 className="text-4xl font-bold mb-4">Hey! I am Puneet</h1>
+          <p className="text-lg">Tech enthusiast, Love building things</p>
+        </div>
+        <div></div>
+      </div>
+    </main>
+  );
+}
