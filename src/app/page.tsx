82e16012a375a77dab4ec795a7aaeedf42d439d9
Puneet56 <puneetdhiman56@gmail.com>
2024-08-01 22:10:19 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,42 @@
+import {
+  EnvelopeClosedIcon,
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+} from '@radix-ui/react-icons'
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main className="container mx-auto py-20">
-      <div className="flex items-start justify-between">
-        <div>
-          <h1 className="text-4xl font-bold mb-4">Hey! I am Puneet</h1>
-          <p className="text-lg">Tech enthusiast, Love building things</p>
-        </div>
-        <div></div>
+    <main className="container mx-auto py-4">
+      <div className="ml-auto flex w-max items-center justify-center gap-4 font-mono">
+        <Link href="#" className="mt-1 text-lg">
+          blog
+        </Link>
+
+        <Link href="https://github.com/Puneet56" target="_blank">
+          <GitHubLogoIcon className="h-8 w-8" />
+        </Link>
+
+        <Link href="https://www.linkedin.com/in/puneet-dhiman/" target="_blank">
+          <LinkedInLogoIcon className="h-8 w-8" />
+        </Link>
+
+        <Link href="mailto:puneetdhiman56@gmail.com">
+          <EnvelopeClosedIcon className="h-8 w-8" />
+        </Link>
       </div>
+
+      <h1 className="mb-4 mt-48 text-2xl font-bold lg:text-3xl">
+        Hey! I am Puneet
+      </h1>
+      <h2 className="text-4xl">
+        Tech enthusiast <br /> Love building things
+      </h2>
+      {/* <div className="mt-24">
+        <h2 className="flex items-end font-mono text-2xl">
+          projects <ArrowBottomRightIcon className="h-8 w-8" />
+        </h2>
+      </div> */}
     </main>
-  );
+  )
 }
